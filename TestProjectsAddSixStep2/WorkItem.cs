3ef355ef56db51f6dcea9a5403e203bb9cfe11b9
Ravil Minikeev <r.minikeev@vekas-automation.ru>
2025-10-18 13:58:19 +0300

    Add project files.

---
+++ TestProjectsAddSixStep2/WorkItem.cs
@@ -0,0 +1,10 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace TestProjectsAddSixStep2;
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
